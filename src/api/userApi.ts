c8fa7a2c618fc4dbbd514294267d4fddf52c15f4
Hkong2 <letranhongkonghpt01@gmail.com>
2025-11-09 21:41:21 +0700

    ...

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000'; // backend NestJS URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (data: { email: string; password: string }) => {
     const response = await axios.post(`${API_URL}/user/register`, data);
